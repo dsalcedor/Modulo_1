69bf5caced6cd1f43d24c988b392575d7a8569a2
DARIO SALCEDO <DARIO_SALCEDO@jabil.com>
2022-12-10 03:22:23 -0600

    Faltan dos funciones

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,18 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  let array = [1];
+  let i = 2;
 
+  while(num !== 1){
+    if(num % i === 0){
+      array.push(i);
+      num = num/i;
+    }else{
+      i++;
+    }
+  }
+  return array;
 }
 
 function bubbleSort(array) {
@@ -14,7 +25,19 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let cambio = true;
+  while(cambio){
+    cambio = false;
+    for(let i=0; i < array.length-1; i++){
+      if(array[i] > array[i+1]){
+        let aux = array[i];
+        array[i] = array[i+1];
+        array[i+1] = aux;
+        cambio = true;
+      }
+    }
+  }
+  return array;
 }
 
 
